edfff39b63687002747165a253b7cf102a530ceb
marcoaam <marco.army@gmail.com>
2014-07-28 17:26:12 +0100

    User management in progress

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'sinatra'
+gem 'shotgun'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+gem 'capybara'
+
+group :development, :test do
+  gem "rspec"
+ end
---
+++ lib/link.rb
@@ -0,0 +1,22 @@
+class Link
+
+	include DataMapper::Resource
+
+	has n, :tags, :through => Resource
+
+	property :id,     Serial
+	property :title,  String
+	property :url,    String
+
+end
+
+class Tag
+
+	include DataMapper::Resource
+
+	has n, :links, :through => Resource
+
+	property :id,   Serial
+	property :text, String
+
+end
