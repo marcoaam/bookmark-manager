40451b33fcada39a5774a7ebaedd05861602bf64
marcoaam <marco.army@gmail.com>
2014-07-29 11:34:55 +0100

    Sign up errors for user

--- lib/user.rb
+++ lib/user.rb
@@ -8,16 +8,16 @@ class User
 
   include DataMapper::Resource
 
-  property :id, Serial
-  property :email, String
+  validates_confirmation_of :password
+  validates_uniqueness_of :email
 
+  property :id, Serial
+  property :email, String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
   def password=(password)
   	@password = password
     self.password_digest = BCrypt::Password.create(password)
   end
-
-  validates_confirmation_of :password
 
 end
